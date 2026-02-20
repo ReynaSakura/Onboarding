0426991cb50f0143619ff970b31fd9ad7bf25b14
ReynaSakura <dorating0829@gmail.com>
2026-02-20 23:10:45 +1100

    PUT (Update)

--- backend/dotnet-Basic/Controllers/StockController.cs
+++ backend/dotnet-Basic/Controllers/StockController.cs
@@ -45,4 +45,29 @@ public class StockController: ControllerBase
         _context.SaveChanges();
         return CreatedAtAction(nameof(GetById), new { id = stockModel.Id}, stockModel.ToStockDto());
     }
+
+    [HttpPut]
+    [Route("{id}")]
+
+    public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+    {
+        var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+        if (stockModel == null)
+        {
+            return NotFound();
+        }
+
+        stockModel.Symbol = updateDto.Symbol;
+        stockModel.CompanyName = updateDto.CompanyName;
+        stockModel.MyProperty = updateDto.MyProperty;
+        stockModel.LastDiv = updateDto.LastDiv;
+        stockModel.Industry = updateDto.Industry;
+        stockModel.MarketCap = updateDto.MarketCap;
+        
+        _context.SaveChanges();
+        return  Ok(stockModel.ToStockDto());
+
+    }
+    
 }
