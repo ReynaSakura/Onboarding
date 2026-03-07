f1f184a181c6047900922a687b0f4ec1d6b2ceaf
ReynaSakura <dorating0829@gmail.com>
2026-03-07 23:49:27 +1100

    Data Migration and data Seeding

--- backend/VideoGameApi/Data/VideoGameDbContext.cs
+++ backend/VideoGameApi/Data/VideoGameDbContext.cs
@@ -1,8 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 namespace VideoGameApi.Data;
+    public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
+    {
+        public DbSet<VideoGame> VideoGames => Set<VideoGame>();
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
-{
-    public DbSet<VideoGame> VideoGames => Set<VideoGame>();
-}
+            modelBuilder.Entity<VideoGame>().HasData(
+                new VideoGame
+                {
+                    Id = 1,
+                    Title = "Genshin Impact",
+                    Platform = "Laptop",
+                    Developer = "Hoyoverse",
+                    Publisher = "Mihoyo",
+                },
+                new VideoGame
+                {
+                    Id = 2,
+                    Title = "The Legend of Zelda",
+                    Platform = "Nintendo Switch",
+                    Developer = "Nintendo EPD",
+                    Publisher = "Nintendo"
+                },
+                new VideoGame
+                {
+                    Id = 3,
+                    Title = "Love and Deepspace",
+                    Platform = "Mobile",
+                    Developer = "Infold Games",
+                    Publisher = "Infold Games"
+                }
+            );
+
+        }
+    }
---
+++ backend/VideoGameApi/Migrations/20260307124351_Seeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VideoGameApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "VideoGames",
+                columns: new[] { "Id", "Developer", "Platform", "Publisher", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Hoyoverse", "Laptop", "Mihoyo", "Genshin Impact" },
+                    { 2, "Nintendo EPD", "Nintendo Switch", "Nintendo", "The Legend of Zelda" },
+                    { 3, "Infold Games", "Mobile", "Infold Games", "Love and Deepspace" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
